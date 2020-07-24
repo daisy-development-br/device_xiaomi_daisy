1547b55dd7c6a8776b1e60d4cc99669c0a5816f0
vitor00almei <pondebatata@outlook.com>
2020-07-23 22:02:39 -0300

    daisy: p404 bring-up

--- p404_daisy.mk
+++ p404_daisy.mk
@@ -21,12 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project-404 stuff.
+$(call inherit-product, vendor/404/configs/common.mk)
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := lineage_daisy
+PRODUCT_NAME := p404_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 Lite
